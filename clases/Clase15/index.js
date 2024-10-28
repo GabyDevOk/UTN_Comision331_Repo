3cc68828e1633eb318a6a7858eacfc50a1edcbcf
GabyDevOk <gabrielmunoz.radio@gmail.com>
2024-10-28 19:25:07 -0300

    modulos?

---
+++ clases/Clase15/index.js
@@ -0,0 +1,6 @@
+const series = require("./series.js") // modulo propio
+const fs = require ("fs") // modulo nativo
+const moment = require ("moment")     // modulo de terceros (NPM)
+
+/* const ahora = moment()
+console.log(ahora.format("DD-MM-YYYY")); */
---
+++ clases/Clase15/series.js
@@ -0,0 +1,10 @@
+const series = [
+    {titulo: "the Office",temporadas:7},
+    {titulo: "Breaking Bad", temporadas :5},
+    {titulo : "Stranger Things", temporadas :4}
+]
+
+module.exports = series
+
+
+
